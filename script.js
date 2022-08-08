decc20d9f9535daac627d0aef27bb5896e07b756
Abdullah Navid <102864939+aabdullah27182845@users.noreply.github.com>
2022-08-08 19:24:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const one = document.getElementById('one')
+const two = document.getElementById('two').childNodes
+const three = document.getElementById('three')
+const four = document.getElementById('four').childNodes
+const five = document.getElementById('five')
+const six = document.getElementById('six').childNodes
+const seven = document.getElementById('seven').childNodes
+
+window.addEventListener('scroll', createAnimation)
+
+console.log(two)
+console.log(four)
+console.log(six)
+console.log(seven)
+
+function createAnimation() {
+	
+}
